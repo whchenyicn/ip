a5f7c14fe87962d5ec99d762824c355e68740bf2
whchenyicn <whchenyicn@gmail.com>
2025-08-21 23:27:09 +0900

    Level-2: Add tasks and list them

--- src/main/java/whchenyicn.java
+++ src/main/java/whchenyicn.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class whchenyicn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tlist = new String[100];
+        int tcount = 0;
         String hline = "__________________________________________________";
         System.out.println(hline);
         System.out.println("Hello! I'm whchenyicn");
@@ -11,14 +13,23 @@ public class whchenyicn {
 
         while (true) {
             String s = sc.nextLine();
-            if (s.equals("bye") || s.equals("Bye")){
+            if (s.equals("bye") || s.equals("Bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 System.out.println(hline);
                 break;
             }
-            System.out.println(hline);
-            System.out.println("    " + s);
-            System.out.println(hline);
+            if (s.equals("list")) {
+                for (int i = 0; i < tcount; i++) {
+                    int number = i + 1;
+                    System.out.println(number + ". " + tlist[i]);
+                }
+            } else {
+                tlist[tcount] = s;
+                tcount += 1;
+                System.out.println(hline);
+                System.out.println("added: " + s);
+                System.out.println(hline);
+            }
         }
     }
 }
