d2155a947d7fd28c2bc2d6729a5afdf7c4d64d4d
whchenyicn <whchenyicn@gmail.com>
2025-08-21 23:18:47 +0900

    Level-1: echo and bye

--- src/main/java/whchenyicn.java
+++ src/main/java/whchenyicn.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class whchenyicn {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String hline = "__________________________________________________";
         System.out.println(hline);
         System.out.println("Hello! I'm whchenyicn");
         System.out.println("What can I do for you?");
         System.out.println(hline);
-        System.out.println("Bye! Hope to see you again soon!");
+
+        while (true) {
+            String s = sc.nextLine();
+            if (s.equals("bye") || s.equals("Bye")){
+                System.out.println("Bye! Hope to see you again soon!");
+                System.out.println(hline);
+                break;
+            }
+            System.out.println(hline);
+            System.out.println("    " + s);
+            System.out.println(hline);
+        }
     }
 }
