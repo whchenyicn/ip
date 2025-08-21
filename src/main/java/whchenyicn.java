a97c4990d552a76a942aed4505be7becada2b03e
whchenyicn <whchenyicn@gmail.com>
2025-08-21 23:06:11 +0900

    Implemented Level-0

---
+++ src/main/java/whchenyicn.java
@@ -0,0 +1,10 @@
+public class whchenyicn {
+    public static void main(String[] args) {
+        String hline = "__________________________________________________";
+        System.out.println(hline);
+        System.out.println("Hello! I'm whchenyicn");
+        System.out.println("What can I do for you?");
+        System.out.println(hline);
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+}
