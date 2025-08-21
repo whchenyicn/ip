a6cb4210b5c804e4ebb802473c7dbc7374475bd5
whchenyicn <whchenyicn@gmail.com>
2025-08-21 23:49:13 +0900

    Level-3: mark and unmark

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String desc;
+    private boolean isDone;
+
+    public Task(String desc) {
+        this.desc = desc;
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return desc;
+    }
+}
--- src/main/java/whchenyicn.java
+++ src/main/java/whchenyicn.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class whchenyicn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] tlist = new String[100];
+        Task[] tlist = new Task[100];
+        boolean[] doneList = new boolean[100];
         int tcount = 0;
         String hline = "__________________________________________________";
         System.out.println(hline);
@@ -13,22 +14,48 @@ public class whchenyicn {
 
         while (true) {
             String s = sc.nextLine();
+            Task t = new Task(s);
+            //terminate with bye
             if (s.equals("bye") || s.equals("Bye")) {
                 System.out.println("Bye! Hope to see you again soon!");
                 System.out.println(hline);
                 break;
-            }
-            if (s.equals("list")) {
-                for (int i = 0; i < tcount; i++) {
-                    int number = i + 1;
-                    System.out.println(number + ". " + tlist[i]);
-                }
             } else {
-                tlist[tcount] = s;
-                tcount += 1;
-                System.out.println(hline);
-                System.out.println("added: " + s);
-                System.out.println(hline);
+                //mark as done
+                if (s.startsWith("mark")) {
+                    Integer i = Integer.parseInt(s.substring(5).trim());
+                    tlist[i - 1].markDone();
+                    System.out.println(hline);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[" + tlist[i - 1].getStatus() + "]" + tlist[i - 1].toString());
+                    System.out.println(hline);
+                } else {
+                    //mark as undone
+                    if (s.startsWith("unmark")) {
+                        Integer i = Integer.parseInt(s.substring(7).trim());
+                        tlist[i - 1].markDone();
+                        System.out.println(hline);
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("[" + tlist[i - 1].getStatus() + "]" + tlist[i - 1].toString());
+                        System.out.println(hline);
+                    } else {
+                        // Listing out everything
+                        if (s.equals("list")) {
+                            System.out.println(hline);
+                            for (int i = 0; i < tcount; i++) {
+                                int number = i + 1;
+                                System.out.println(number + ". [" + tlist[i].getStatus() + "] " + tlist[i].toString());
+                            }
+                            System.out.println(hline);
+                        } else {
+                            tlist[tcount] = t;
+                            tcount += 1;
+                            System.out.println(hline);
+                            System.out.println("added: " + s);
+                            System.out.println(hline);
+                        }
+                    }
+                }
             }
         }
     }
